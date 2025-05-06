bf50764799d719fabaebb9b265a313921c529170
Akash <bhandariaakash675@gmail.com>
2025-05-06 12:30:26 +0530

    DAY-4

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.example.demo.controller;
+import com.example.demo.entity.User;
+import com.example.demo.serviceImpl.UserServiceImpl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	private UserServiceImpl userService;
+	
+	@PostMapping("/register")
+	public User createUser(@RequestBody User user)  
+	{
+		return userService.createUser(user);
+	}
+	@GetMapping("/all")
+	public List<User> getAllUsers()
+	{
+		return userService.getAllUsers();
+	}
+	
+	@PutMapping("/update/{id}")
+	public User updateUser(@PathVariable String id,@RequestBody User user)
+	{
+		return userService.updateUser(id,user);
+	}
+	@DeleteMapping("/delete/{id}")
+	public User deleteUser(@PathVariable String id)
+	{
+		return userService.deleteUser(id);
+	}
+}
