34b85ec0f9ae14b81b2c71c2b94ff83ea755ed23
Akash <bhandariaakash675@gmail.com>
2025-05-06 12:33:17 +0530

    DAY-4

--- src/main/java/com/example/demo/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/example/demo/serviceImpl/UserServiceImpl.java
@@ -14,8 +14,38 @@ public class UserServiceImpl implements UserService{
 
 	@Override
 	public User createUser(User user) {
-		System.out.println("Service Class working");
 		a1.add(user);
 		return user;
+	}
+	
+	@Override
+	public List<User> getAllUsers(){
+		return a1;
+	}
+
+	public User updateUser(String id,User user) {
+		
+		for(User u:a1)
+		{
+			if(u.getId().equals(id))
+			{
+				u.setUserName(user.getName());
+				u.setName(user.getUserName());
+				return u;
+			}
+		}
+		return null;
+	}
+	public User deleteUser(String id) {
+		
+		for(User u:a1)
+		{
+			if(u.getId().equals(id))
+			{
+				a1.remove(u);
+				return u;
+			}
+		}
+		return null;
 	}
 }
