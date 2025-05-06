e1e3178582942387fd04259351021959d0ec4097
Akash <bhandariaakash675@gmail.com>
2025-05-06 12:32:49 +0530

    DAY-4

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.User;
@@ -8,4 +10,12 @@ import com.example.demo.entity.User;
 public interface UserService {
 
 	User createUser(User user);
+
+	List<User> getAllUsers();
+	
+	User updateUser(String id,User user);
+	
+	User deleteUser(String id);
+	
+	
 }
