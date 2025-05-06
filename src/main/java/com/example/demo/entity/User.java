67e0d5f0c5fe5aed12f898e27d9ab1574f70bdfb
Akash <bhandariaakash675@gmail.com>
2025-05-06 12:32:20 +0530

    DAY-4

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -2,32 +2,45 @@ package com.example.demo.entity;
 
 public class User {
 
+	private String id;
 	private String name;
-	String userName;
-	String password;
+	private String userName;
+	private String password;
+	
 	@Override
 	public String toString() {
-		return "User [name=" + name + ", userName=" + userName + ", password=" + password + "]";
+		return "User [id=" + id + ", name=" + name + ", userName=" + userName + ", password=" + password + "]";
 	}
-	
-	public User(String name, String userName, String password) {
+	public User(String id, String name, String userName, String password) {
 		super();
+		this.id = id;
 		this.name = name;
 		this.userName = userName;
-		this.password = password;
+		this.password = password;	
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public String getUserName() {
 		return userName;
 	}
-
-	public String getPassword() {
-		return password;
+	public void setId(String id) {
+	    this.id = id;
 	}
+	public void setPassword(String password) {
+        this.password = password;
+    }
+	public void setName(String name) {
+		
+		this.name = name;
+	}
+	public void setUserName(String userName) {
+	    this.userName = userName;
+	}
+	public String getId() {
+        return id;
+    }
+
 
 	
 }
